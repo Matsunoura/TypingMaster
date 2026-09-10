ec61e49d015612d23199e4875fc79823ee803038
Matsunoura <tokagenobiru@gmail.com>
2026-09-10 11:09:05 +0900

    update

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -5,6 +5,7 @@ const contentToCache = [
     "Build/Build.data",
     "Build/Build.wasm",
     "TemplateData/style.css"
+
 ];
 
 self.addEventListener('install', function (e) {
@@ -18,23 +19,12 @@ self.addEventListener('install', function (e) {
 });
 
 self.addEventListener('fetch', function (e) {
-    // http または https 以外のリクエスト（chrome-extension等）はキャッシュ処理をスルーする
-    if (!e.request.url.startsWith('http')) {
-        return;
-    }
-
     e.respondWith((async function () {
       let response = await caches.match(e.request);
       console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
       if (response) { return response; }
 
       response = await fetch(e.request);
-      
-      // レスポンスが正常でない場合や http(s) 以外の場合はキャッシュしない
-      if (!response || response.status !== 200 || response.type !== 'basic') {
-          return response;
-      }
-
       const cache = await caches.open(cacheName);
       console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
       cache.put(e.request, response.clone());
